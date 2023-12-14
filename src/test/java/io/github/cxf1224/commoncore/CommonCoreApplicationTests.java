6d629fad73234b7d581b2e3a42e3b72214b50d8c
cui xingfan <cuixingfan@reddatetech.com>
2023-12-14 16:45:47 +0800

    Initial commit

---
+++ src/test/java/io/github/cxf1224/commoncore/CommonCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cxf1224.commoncore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonCoreApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
